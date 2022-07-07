21a2f9efd116a89afb7010e15f27429c940158ba
Charlene Auger <charlene.auger@factorfx.com>
2022-07-07 14:54:27 +0200

    Update Map.pm

--- APACHE/Map.pm
+++ APACHE/Map.pm
@@ -5,16 +5,18 @@ use strict;
 use Apache::Ocsinventory::Map;
 
 $DATA_MAP{userinstalledapps} = {
-		mask => 0,
-		multi => 1,
-		auto => 1,
-		delOnReplace => 1,
-		sortBy => 'PATH',
-		writeDiff => 0,
-		cache => 0,
-		fields => {
-                USERNAME => {},
-                APPNAME => {}
+	mask => 0,
+	multi => 1,
+	auto => 1,
+	delOnReplace => 1,
+	sortBy => 'PATH',
+	writeDiff => 0,
+	cache => 0,
+	fields => {
+		USERNAME => {},
+		APPNAME => {},
+		PUBLISHER => {},
+		VERSION => {}
 	}
 };
 1;
